93140526553898a4fce85de2c2100e157360f0de
Pankhudi <bpankhudi7@gmail.com>
2021-05-07 17:42:19 +0530

    Implement movie rental statement in html format for customer

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Customer {
+    private static final String H3_OPEN_TAG = "<h3>";
+    private static final String H3_CLOSING_TAG = "</h3>";
+    private static final String BOLD_OPEN_TAG = "<b>";
+    private static final String BOLD_CLOSING_TAG = "</b>";
+    private static final String PARAGRAPH_OPEN_TAG = "<p>";
+    private static final String PARAGRAPH_CLOSE_TAG = "</p>";
+    private static final String LINE_BREAK_TAG = "<br/>";
+
     private String name;
     private List<Rental> rentals = new ArrayList<>();
 
@@ -29,6 +37,19 @@ public class Customer {
         result += "Amount owed is " + totalAmount() + "\n";
         result += "You earned " + totalFrequentRenterPoints
                 + " frequent renter points";
+        return result;
+    }
+
+    public String htmlStatement() {
+        String result = H3_OPEN_TAG + "Rental Record for " + getName() + H3_CLOSING_TAG + LINE_BREAK_TAG;
+        result += PARAGRAPH_OPEN_TAG;
+        for (Rental each : rentals) {
+            result += each.getMovie().getTitle() + BOLD_OPEN_TAG + each.amount() + BOLD_CLOSING_TAG + LINE_BREAK_TAG;
+        }
+        result += PARAGRAPH_CLOSE_TAG;
+        int totalFrequentRenterPoints = totalFrequentRenterPoints();
+        result += PARAGRAPH_OPEN_TAG + "Amount owed is " + BOLD_OPEN_TAG + totalAmount() + BOLD_CLOSING_TAG + PARAGRAPH_CLOSE_TAG + LINE_BREAK_TAG;
+        result += PARAGRAPH_OPEN_TAG + "You earned " + BOLD_OPEN_TAG + totalFrequentRenterPoints + BOLD_CLOSING_TAG + " frequent renter points" + PARAGRAPH_CLOSE_TAG;
         return result;
     }
 
--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -37,4 +37,31 @@ public class CustomerTest {
                 "Amount owed is 11.0\n" +
                 "You earned 3 frequent renter points", statement);
     }
+
+    @Test
+    public void shouldGenerateHTMLStatementForCustomer() {
+        Customer customer = new Customer("Pankhudi Bhosle");
+
+        Movie batman = new Movie("Batman", Movie.REGULAR);
+        Rental batmanRental = new Rental(batman, 3);
+
+        Movie stuartLittle = new Movie("Stuart Little", Movie.CHILDRENS);
+        Rental stuartLittleRental = new Rental(stuartLittle, 5);
+
+        Movie starWars = new Movie("Star Wars", Movie.NEW_RELEASE);
+        Rental starWarsRental = new Rental(starWars, 1);
+
+        customer.addRental(batmanRental);
+        customer.addRental(stuartLittleRental);
+        customer.addRental(starWarsRental);
+
+        String statement = customer.htmlStatement();
+        Assert.assertEquals("<h3>Rental Record for Pankhudi Bhosle</h3><br/>" +
+                "<p>Batman<b>3.5</b><br/>" +
+                "Stuart Little<b>4.5</b><br/>" +
+                "Star Wars<b>3.0</b><br/>" +
+                "</p><p>Amount owed is <b>11.0</b></p><br/>" +
+                "<p>You earned <b>3</b> frequent renter points</p>", statement);
+    }
+
 }

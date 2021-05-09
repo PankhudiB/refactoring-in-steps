60ffea5c3f0f1926e3593053e2017f6246777551
Pankhudi <bpankhudi7@gmail.com>
2021-05-09 09:14:24 +0530

    Refactor to remove feature envy of Rental class towards Movie

--- src/main/java/com/thoughtworks/movierental/Movie.java
+++ src/main/java/com/thoughtworks/movierental/Movie.java
@@ -25,4 +25,38 @@ public class Movie {
         return title;
     }
 
+    double amountFor(int daysRented) {
+        double thisAmount = 0;
+        switch (getPriceCode()) {
+            case REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
+
+    int frequentRenterPointsFor(int daysRented) {
+        int frequentRenterPoints = 0;
+        frequentRenterPoints++;
+        frequentRenterPoints += applicableBonus(daysRented);
+        return frequentRenterPoints;
+    }
+
+    private int applicableBonus(int daysRented) {
+        if ((this.getPriceCode() == Movie.NEW_RELEASE)
+                &&
+                daysRented > 1) return 1;
+        return 0;
+    }
+
 }
--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -19,37 +19,11 @@ public class Rental {
     }
 
     public double amount() {
-        double thisAmount = 0;
-        switch (movie.getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2)
-                    thisAmount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (daysRented > 3)
-                    thisAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
+        return movie.amountFor(this.daysRented);
     }
 
     public int frequentRenterPoints() {
-        int frequentRenterPoints = 0;
-        frequentRenterPoints++;
-        frequentRenterPoints += applicableBonus();
-        return frequentRenterPoints;
-    }
-
-    private int applicableBonus() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                &&
-                getDaysRented() > 1) return 1;
-        return 0;
+        return movie.frequentRenterPointsFor(this.daysRented);
     }
 
     String title() {
